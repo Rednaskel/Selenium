9865ba28a1fd26dcc915501c8377e471d40932ad
Rednaskel <kosinski.aleksander@gmail.com>
2015-01-02 00:02:01 +0100

    Little driver sth

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.*;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
 
 public class Main {
 
 	public static void main(String[] args)
 	{
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.navigate().to("http://mail.google.com");
+		
+		driver.findElement(By.id("Email")).sendKeys("kosinski.aleksander@gmail.com");
+		driver.findElement(By.id("Passwd")).sendKeys("XxX");
+		driver.findElement(By.id("signIn")).click();
+		
+		
 		
 	}
 	
